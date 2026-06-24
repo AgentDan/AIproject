7db076b48e74ffd903972ca4c53fec54bf1f2d4f
AgentDan <bion-1@mail.ru>
2026-06-24 20:44:40 +0200

    com

--- apps/server/src/core/orchestrator.js
+++ apps/server/src/core/orchestrator.js
@@ -84,8 +84,6 @@ export async function orchestrateCommand(clientRequest) {
   const { sceneContext, validationErrors: sceneContextErrors } =
     await buildSceneContext(clientRequest);
 
-    console.log('sceneContext: ', sceneContext);
-
   if (sceneContextErrors.length > 0) {
     return {
       statusCode: 500,
